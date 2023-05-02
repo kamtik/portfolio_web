d9dddf14e2fe958ad175ae0426ec9193e11e3903
Tik Kam <T.Kam@wlv.ac.uk>
2023-05-02 13:30:33 +0800

    Modified api/db.sql file for added index, more realistic sample data, added webp images

--- api/db.sql
+++ api/db.sql
@@ -1,6 +1,5 @@
 CREATE TABLE `movie` (
   `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `cat_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `img` varchar(255) NOT NULL,
@@ -16,15 +15,28 @@ CREATE TABLE `feedback` (
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE UNIQUE INDEX idx_movie_id ON movie(id);
+CREATE UNIQUE INDEX idx_feedback_id ON feedback(id);
+CREATE INDEX idx_feedback_movie_id ON feedback(movie_id);
 
-insert into movie(cat_id,name,description,img) values(1,'Test','Desc','test.jpg');
-insert into movie(cat_id,name,description,img) values(1,'Test2','Desc2','');
-insert into feedback(movie_id,name,rating,comment) values(1,'Tester',8,'test comment');
-insert into feedback(movie_id,name,rating,comment) values(1,'Tester2',4,'test comment 2');
 
-insert into category(name) values('2023');
+
+insert into movie(name,description,img) values('The Final','When it comes to final.... what would be the humanity\'s last hope.','');
+insert into movie(name,description,img) values('The best day','Here it comes the best day when...','6.webp');
+insert into movie(name,description,img) values('Untitled','This is an untitiled movie. As its name, it is just untitled','');
+insert into movie(name,description,img) values('Infinity sky','A sky that loop infinitely, loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely,loop infinitely','4.webp');
+insert into movie(name,description,img) values('Holiday Mood','A man in holiday','1.webp');
+insert into movie(name,description,img) values('The fireman','Detective Harry Hole investigates the disappearance of a woman whose scarf is found wrapped around an ominous-looking fireman.','8.webp');
+insert into movie(name,description,img) values('Super Unnatural','Two brothers follow their father\'s footsteps as hunters, fighting evil supernatural beings of many kinds, including monsters, demons, and gods that roam the earth.','7.webp');
+insert into movie(name,description,img) values('The Girls','A group of vigilantes set out to take down corrupt superheroes who abuse their superpowers.','3.webp');
+insert into movie(name,description,img) values('The Last of Me','After a global pandemic destroys civilization, a hardened survivor takes charge of a 14-year-old girl who may be humanity\'s last hope.','2.webp');
+insert into movie(name,description,img) values('FlyMan','With FlyMan\'s identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be FlyMan.','5.webp');
+
+insert into feedback(movie_id,name,rating,comment) values(10,'Mr Bean',8,'Good Movie!');
+insert into feedback(movie_id,name,rating,comment) values(10,'Robo',5,'Not bad');
+insert into feedback(movie_id,name,rating,comment) values(10,'Mary',1,'Rubbish');
+insert into feedback(movie_id,name,rating,comment) values(10,'Peter',10,'Excellent! This is the best movie I have ever watched. I hope there will be similar movie coming soon. I will never forget this movie.');
+
+insert into feedback(movie_id,name,rating,comment) values(8,'Paul',5,'Not bad');
+insert into feedback(movie_id,name,rating,comment) values(6,'Someone',7,'Nice!');
+insert into feedback(movie_id,name,rating,comment) values(6,'Bob',2,'This movie is terrible');
